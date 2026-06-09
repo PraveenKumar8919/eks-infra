35e7482e2c337e9d745c5747b3e0fc31dc4d0190
PraveenKumar <135730649+PraveenKumar8919@users.noreply.github.com>
2026-06-09 15:06:34 -0400

    switch from Auto Mode to Spot managed node group
    - Replaced compute_config (Auto Mode) with eks_managed_node_groups
    - Spot instances: t3.xlarge, t3a.xlarge, m5.xlarge, m5a.xlarge
    - Multiple instance types for better Spot availability and fewer interruptions
    - min 2 / max 5 nodes, desired 2

--- main.tf
+++ main.tf
@@ -8,13 +8,32 @@ module "eks" {
   endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
 
-  compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
-  }
-
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
+
+  eks_managed_node_groups = {
+    spot = {
+      # Multiple instance types = more Spot capacity pools = fewer interruptions
+      # t3/t3a.xlarge: 4 vCPU 16GB  |  m5/m5a.xlarge: 4 vCPU 16GB
+      instance_types = [
+        "t3.xlarge",
+        "t3a.xlarge",
+        "m5.xlarge",
+        "m5a.xlarge",
+      ]
+
+      capacity_type = "SPOT"
+
+      min_size     = 2
+      max_size     = 5
+      desired_size = 2
+
+      labels = {
+        role            = "spot-worker"
+        lifecycle       = "spot"
+      }
+    }
+  }
 
   tags = {
     Environment = "test"
